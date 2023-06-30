df7c7ee5eb044a073288fb71dab645086cffca05
saurav2021c <saurav2021c@gmail.com>
2023-07-01 01:06:37 +0530

    About Me Completed

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from './components/Navbar';
 import Header from './components/Header';
-
+import AboutMe from './components/AboutMe';
 function App() {
   return (
     <>
     <Navbar />
     <Header />
+    <AboutMe />
     </>
   );
 }
