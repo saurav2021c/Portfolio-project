73cb471122f5021cb982f79b5c4e33cc9d9dc28a
saurav2021c <saurav2021c@gmail.com>
2023-07-03 00:35:59 +0530

    Contacts Component Completed

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import AboutMe from './components/AboutMe';
 import Skills from './components/Skills';
 import Experience from './components/Experience';
 import Projects from './components/Projects';
+import Contacts from './components/Contacts';
 function App() {
   return (
     <>
@@ -15,6 +16,7 @@ function App() {
     <Skills />
     <Experience />
     <Projects />
+    <Contacts />
     </>
   );
 }
